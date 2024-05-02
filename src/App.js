6aec3695fff70427af6138dd101c57cdf3e9c4fd
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-05-02 20:55:41 -0300

    feat: aplicando props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Container from "./components/Container";
 import './App.css';
 
 function App() {
+	const [number, setNumber]= useState(10);
+	const handleMessage = (number)=>{
+		setNumber (20);
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+			<h1>Curso React {number}</h1>
+
+			<button onClick={handleMessage}>mudar numero</button>
+
+			<Container brand="VW" color="Vermelho" km={535} />
+		</div>
+
   );
 }
 
