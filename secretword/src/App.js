3ebf9d23d422e7e4af5ea94b74b2eb2721f06126
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-05-07 19:33:01 -0300

    feat:criando componentes

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
+import { useCallback, useEffect, useState } from "react";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+// data
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 
 function App() {
+	const [gameStage, setGameStage] = useState(stages[0].name);
   return (
     <div className="App">
-      <h2> Secret word</h2>
+      {gameStage === "start" &&< StartScreen/>}
+      {gameStage === "game" &&< Game/>}
+      {gameStage === "end" &&< GameOver/>}
     </div>
   );
 }
